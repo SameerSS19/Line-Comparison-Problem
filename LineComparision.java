da87e3c156e9d4ee0f36bb91c0afcb99211250b4
unknown <sam1919>
2021-05-13 12:57:17 +0530

    Compare two line Equal_GraterOrless

--- LineComparision.java
+++ LineComparision.java
@@ -10,11 +10,12 @@ public class LineComparision
 		System.out.println("Welcome to Line Comparison Computation Program");
 	
 		double x1, x2, y1, y2, LengthofLine,LengthofLine2;    //Declaration
-
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the value of x1");
 		
-		x1 = scan.nextDouble();   //Taking input from user x & y
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the value for Line 1");
+		
+		System.out.println("Enter the value of x1");  //Taking input from user x & y
+		x1 = scan.nextDouble();   
 		System.out.println("Enter the value of x2");
 		x2 = scan.nextDouble();
 		System.out.println("Enter the value of y1");
@@ -24,7 +25,7 @@ public class LineComparision
 		LengthofLine = Math.sqrt(  (Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)) );
 		System.out.println(LengthofLine);
 		
-		
+		System.out.println("Enter the values for Line 2");
 		System.out.println("Enter the value of x1"); //Taking input from user x & y
 		x1 = scan.nextDouble();  
 		System.out.println("Enter the value of x2");
@@ -34,13 +35,13 @@ public class LineComparision
 		System.out.println("Enter the value of y2");
 		y2 = scan.nextDouble();
 		LengthofLine2 = Math.sqrt(  (Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)) );
-		System.out.println(LengthofLine);
+		System.out.println("LengthofLine2:" + LengthofLine2);
 		
 		String s1,s2;
 		s1=Double.toString(LengthofLine);
 		s2=Double.toString(LengthofLine2);
 		System.out.println("Equality of two lines:" + s1.equals(s2));
-		
+		System.out.println("Compare of two length is:" +s1.compareTo(s2));
       }
  }
 
