88588fa2b0946345d8ba2bfc5dee8d1c4446f661
unknown <sam1919>
2021-05-13 12:38:55 +0530

    Check Two Line are Equal

--- LineComparision.java
+++ LineComparision.java
@@ -1,6 +1,7 @@
 package com.linecompareprblm;
 
 import java.util.Scanner;
+import java.util.lang.*;
 public class LineComparision 
 {
 	public static void main(String[] args) 
@@ -8,10 +9,12 @@ public class LineComparision
 	
 		System.out.println("Welcome to Line Comparison Computation Program");
 	
-		double x1, x2, y1, y2, LengthofLine;
+		double x1, x2, y1, y2, LengthofLine,LengthofLine2;    //Declaration
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the value of x1");
-		x1 = scan.nextDouble();
+		
+		x1 = scan.nextDouble();   //Taking input from user x & y
 		System.out.println("Enter the value of x2");
 		x2 = scan.nextDouble();
 		System.out.println("Enter the value of y1");
@@ -20,7 +23,24 @@ public class LineComparision
 		y2 = scan.nextDouble();
 		LengthofLine = Math.sqrt(  (Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)) );
 		System.out.println(LengthofLine);
+		
+		
+		System.out.println("Enter the value of x1"); //Taking input from user x & y
+		x1 = scan.nextDouble();  
+		System.out.println("Enter the value of x2");
+		x2 = scan.nextDouble();
+		System.out.println("Enter the value of y1");
+		y1 = scan.nextDouble();
+		System.out.println("Enter the value of y2");
+		y2 = scan.nextDouble();
+		LengthofLine2 = Math.sqrt(  (Math.pow((x2-x1),2))  + (Math.pow((y2-y1),2)) );
 		System.out.println(LengthofLine);
+		
+		String s1,s2;
+		s1=Double.toString(LengthofLine);
+		s2=Double.toString(LengthofLine2);
+		System.out.println("Equality of two lines:" + s1.equals(s2));
+		
       }
  }
 
